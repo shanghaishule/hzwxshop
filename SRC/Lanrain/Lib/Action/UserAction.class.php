b00a137ae28078b8a49dcaebf9655811f13eada7
polo2013 <paul.zhou@163.com>
2014-02-28 11:55:40 +0800

    初始化项目

---
+++ SRC/Lanrain/Lib/Action/UserAction.class.php
@@ -0,0 +1,41 @@
+<?php
+class UserAction extends BaseAction{
+	protected function _initialize(){
+		parent::_initialize();
+		$userinfo=M('User_group')->where(array('id'=>session('gid')))->find();
+		$users=M('Users')->where(array('id'=>$_SESSION['uid']))->find();
+		$this->assign('thisUser',$users);
+		//dump($users);
+		$this->assign('viptime',$users['viptime']);
+		if(session('uid')){
+			if($users['viptime']<time()){
+				session(null);
+				session_destroy();
+				unset($_SESSION);
+				$this->error('您的帐号已经到期，请充值后再使用');
+			}
+		}
+		$wecha=M('Wxuser')->field('wxname,wxid,headerpic,weixin')->where(array('token'=>session('token'),'uid'=>session('uid')))->find();
+		$this->assign('wecha',$wecha);
+		$this->assign('token',session('token'));
+		//
+		$this->assign('userinfo',$userinfo);
+		if(session('uid')==false){
+			$this->redirect('Home/Index/login');
+		}
+	}
+	
+	public function checkauth($funname, $modname){
+		//检查权限
+		$func = M('Function')->where(array('funname'=>$funname))->find();
+		if ($func['gid'] > session('gid')) {
+			$this->error('您没有该模块的使用权限,请升级您的账号！');
+		}
+		
+		//检查模块是否勾选
+		$token_open=M('token_open')->field('queryname')->where(array('token'=>session('token')))->find();
+		if(!strpos($token_open['queryname'],$modname)){
+			$this->error('您还未开启该模块的使用,请到功能模块中添加！',U('Function/index',array('token'=>session('token'),'id'=>session('wxid'))));
+		}
+	}
+}
---
+++ SRC/data/conf/info.php
@@ -0,0 +1,17 @@
+<?php 
+return array (
+  'site_name' => '微指购',
+  'site_title' => '微指购',
+  'site_url' => 'localhost',
+  'home_token' => 'gmbqsf1389536537',
+  'site_my' => '微指购',
+  'ischeckuser' => 'true',
+  'ipc' => '微指购',
+  'site_qq' => '404249022',
+  'baidu_map_api' => '2b79c282b0f365ca9782cceb499e6022',
+  'site_email' => 'service@555.com',
+  'keyword' => '微指购',
+  'content' => '微指购',
+  'counts' => '',
+  'copyright' => '',
+);
