66f020c31f2aa291b6e72ac33aa8131184c61b77
polo2013 <paul.zhou@163.com>
2014-03-17 10:29:50 +0800

    1

--- DOC/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return $arr= array (
-  'DB_HOST' => 'lzoon.gotoftp4.com',
-  'DB_PORT' => '3306',
-  'DB_NAME' => 'lzoon',
-  'DB_USER' => 'lzoon',
-  'DB_PWD' => 'hhj900712llc',
-  'DB_PREFIX' => 'tp_',
-);
