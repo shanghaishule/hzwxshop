2692767da9d975da402924b1a520d22522db1121
polo2013 <paul.zhou@163.com>
2014-03-26 11:01:27 +0800

    2

---
+++ DOC/lanrain/db.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'wxweb',
+  'DB_USER' => 'wxweb',
+  'DB_PWD' => '123#321',
+  'DB_PREFIX' => 'tp_',
+);
+?>
---
+++ DOC/weTall/db.php
@@ -0,0 +1,9 @@
+<?php
+return $arr= array (
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'wxweb',
+  'DB_USER' => 'wxweb',
+  'DB_PWD' => '123#321',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'tp_',
+);
